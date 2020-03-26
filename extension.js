9a8821e6d85b584d423b0ae6dfdcc2fb12a638ce
richanynguon <alina.nguon@gmail.com>
2020-03-26 18:37:45 -0400

    ability to read and create ast

--- extension.js
+++ extension.js
@@ -1,44 +1,37 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const acorn = require("acorn");
+const fs = require("fs");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "parsnips" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-	// 	// The code you place here will be executed every time your command is executed
+	vscode.languages.registerHoverProvider("javascript", {
+		async provideHover(document, position, token) {
+			const docData = await fs.readFileSync(`${document.fileName}`, "utf8")
 
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World!');
-	// });
+			const docNodes = await acorn.parse(docData)
 
-	// context.subscriptions.push(disposable);
+			await console.log(docNodes)
 
-	
+			return {
+				contents: ["Parse your snippets"]
+			};
+		}
+	});
+
+	const hell0 = vscode.window.showInformationMessage(
+		"Hey welcome back sempai 🙇🏻 you can do it"
+	);
+	context.subscriptions.push(hell0);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
 	activate,
 	deactivate
-}
-
-
-
-
-
+};
