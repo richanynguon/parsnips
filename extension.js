f9807f74d32837df6d68e22e8801dd6d9e28c7fb
richanynguon <alina.nguon@gmail.com>
2020-03-26 17:06:37 -0400

    install language dependency and add language identifier

--- extension.js
+++ extension.js
@@ -17,14 +17,16 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
+
+	
 }
 exports.activate = activate;
 
@@ -35,3 +37,8 @@ module.exports = {
 	activate,
 	deactivate
 }
+
+
+
+
+
